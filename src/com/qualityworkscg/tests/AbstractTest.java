9b3151031164208db2077c477e81fae7c0ca56d5
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-12-06 22:16:39 +0100

    Update AbstractTest.java

--- src/com/qualityworkscg/tests/AbstractTest.java
+++ src/com/qualityworkscg/tests/AbstractTest.java
@@ -20,13 +20,19 @@ public abstract class  AbstractTest {
     // Set the path to the geckodriver
    // System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
     System.setProperty("webdriver.edge.driver", "./drivers/msedgedriver.exe");
-   
+   EdgeOptions options = new EdgeOptions();
+		options.addArguments("--remote-debugging-port=9222");
+options.addArguments("--headless");
+		  		  options.addArguments("--no-sandbox");
+		  		  options.addArguments("--disable-setuid-sandbox");
+		  		  options.addArguments("--disable-dev-shm-usage");
+		  		  options.addArguments("--disable-extensions");
     // Instantiate a new Page and navigate 
     // to the url specified in the testng.xml
   //  page = new Page(new FirefoxDriver());
   
 		    	
-    page = new Page(new EdgeDriver());
+    page = new Page(new EdgeDriver(options));
     
     page.navigate(url);
   }
