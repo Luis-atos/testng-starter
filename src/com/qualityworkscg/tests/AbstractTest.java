386bdebc98e7ba1605613dba3c0a3c8ad42489a3
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-12-06 14:15:28 +0100

    Update AbstractTest.java

--- src/com/qualityworkscg/tests/AbstractTest.java
+++ src/com/qualityworkscg/tests/AbstractTest.java
@@ -16,9 +16,8 @@ public abstract class  AbstractTest {
   @Parameters({"url"})
   public void setup(String url) {
     // Set the path to the geckodriver
-    //System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
-    System.setProperty("webdriver.gecko.driver", "C:\Program Files\Mozilla Firefox\firefox.exe");
-    
+    System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+   
     // Instantiate a new Page and navigate 
     // to the url specified in the testng.xml
     page = new Page(new FirefoxDriver());
